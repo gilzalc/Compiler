d924575e3728030519accf67eeea1ff7908dae8e
gilzalc <gilzalcberg96@gmail.com>
2021-01-04 14:55:36 +0200

    gils commit

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -2,6 +2,6 @@ package oop.ex6.main;
 
 public class Sjavac {
     public static void main(String[] args) {
-        System.out.println("works?,yessss");
+        System.out.println("works?,name_of_your_new_branch");
     }
 }
