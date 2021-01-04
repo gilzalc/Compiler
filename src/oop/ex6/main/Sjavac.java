09efd78d80a6ac9b9e109701892589bac4acf6ed
amichaiv2 <amam7698>
2021-01-04 18:03:02 +0200

    aa

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -4,6 +4,6 @@ public class Sjavac {
     public static void main(String[] args) {
         System.out.println(4);
         System.out.println(44);
-        System.out.println("works?,name_of_your_news_branch");
+        System.out.println("works?,name_of_your_new_branch");
     }
 }
