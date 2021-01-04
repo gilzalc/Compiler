eb5839681ca3b75b46ea5aa050c902d078da98e1
gilzalc <gilzalcberg96@gmail.com>
2021-01-04 13:52:23 +0200

    commit and push

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -2,7 +2,6 @@ package oop.ex6.main;
 
 public class Sjavac {
     public static void main(String[] args) {
-        System.out.println("works?");
-        System.out.println("works?");
+        System.out.println("works?????");
     }
 }
