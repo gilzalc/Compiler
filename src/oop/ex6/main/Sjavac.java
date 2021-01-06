dcb3b759ea7df3c73a60ededfd39dd2de6b125ca
amichaiv2 <amam7698>
2021-01-06 21:55:52 +0200

    .

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -2,7 +2,6 @@ package oop.ex6.main;
 import oop.ex6.Manager;
 import oop.ex6.Reader;
 
-import java.util.regex.*;
 public class Sjavac {
     public static void main(String[] args) {
         Reader reader = new Reader(args[0]);
