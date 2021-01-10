27d06070f75bae2669cc2695c8cd4e863c5155cf
gilzalc <gilzalcberg96@gmail.com>
2021-01-10 23:40:13 +0200

    Scope parser and IfWhile.java

--- src/oop/ex6/parsers/IfWhileParser.java
+++ src/oop/ex6/parsers/IfWhileParser.java
@@ -1,12 +1,13 @@
 package oop.ex6.parsers;
 
 import oop.ex6.ScopeParser;
+import oop.ex6.scopes.IfWhile;
 
 public class IfWhileParser extends ScopeParser {
 	private ScopeParser parentParser;
 
 	public IfWhileParser(ScopeParser parser){
-		super(parser);
+		super(parser,new IfWhile());
 	}
 
 	@Override
--- src/oop/ex6/scopes/IfWhile.java
+++ src/oop/ex6/scopes/IfWhile.java
@@ -1,5 +1,23 @@
 package oop.ex6.scopes;
 
-public class IfWhile extends Scope {
+import oop.ex6.Keywords;
+import oop.ex6.Regex;
+import oop.ex6.Variable;
 
+public class IfWhile extends Scope {
+	private boolean checkCondition(String cond) {
+		if (cond.equals("true") || cond.equals("false")) {
+			return true;
+		}
+		Keywords.Type bool = Keywords.Type.BOOLEAN;
+		if (Regex.isVarNameValid(cond)) { //search first
+			Variable var = getVariable(cond);
+			if (var != null) {
+				return bool.isMatching(var.getType());
+			}
+		}
+		return Regex.isValidVal(bool.getRegex(), cond);
+
+
+	}
 }
