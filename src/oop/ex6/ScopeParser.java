ed90b73ee055b1e153092c3ea0a5347b032b9b8e
amichaiv2 <amam7698>
2021-01-10 20:12:52 +0200

    .

--- src/oop/ex6/ScopeParser.java
+++ src/oop/ex6/ScopeParser.java
@@ -1,18 +1,30 @@
 package oop.ex6;
 
 import oop.ex6.parsers.MethodParser;
+import oop.ex6.parsers.UnInitializedFinalVar;
+import oop.ex6.parsers.UnmatchingValueError;
+import oop.ex6.scopes.Scope;
 
 import java.util.LinkedList;
 
 public abstract class ScopeParser {
+
+	private static final String BOOLEAN = "boolean";
+	private static final String INT = "int";
+	private static final String DOUBLE = "double";
+	private static final String STRING = "String";
+	private static final String CHAR = "char";
 	protected LinkedList<String> scopeLines;
 	protected ScopeParser parentParser;
 	protected LinkedList<ScopeParser> childParsers;
+	protected Scope scope;
 
-	protected ScopeParser(ScopeParser parser){
+	protected ScopeParser(ScopeParser parser, Scope s){
 		parentParser = parser;
+		scope = s;
 		scopeLines = new LinkedList<>();
 		childParsers = new LinkedList<>();
+
 	}
 	public abstract void checkLines() throws IllegalFileFormat;
 
@@ -30,5 +42,54 @@ public abstract class ScopeParser {
 
 	public LinkedList<String> getScopeLines(){
 		return scopeLines;
+	}
+
+	protected Keywords.Type checkVarType(String firstWord) {
+		switch (firstWord) {
+		case BOOLEAN:
+			return Keywords.Type.BOOLEAN;
+		case CHAR:
+			return Keywords.Type.CHAR;
+		case STRING:
+			return Keywords.Type.STRING;
+		case INT:
+			return Keywords.Type.INT;
+		case DOUBLE:
+			return Keywords.Type.DOUBLE;
+		default:
+			return null;
+		}
+	}
+
+	public void createVars(boolean hasFinal, Keywords.Type type, Regex regex)
+			throws UnInitializedFinalVar, UnmatchingValueError, IllegalFileFormat {
+		String[] varDeclarations = regex.splitByComma();
+		for (String declaration : varDeclarations) {
+			String[] str = Regex.getVarNameAndValue(declaration);
+			String nameString = str[0];
+			String valueString = str[1];
+			if (nameString == null || !Regex.isVarNameValid(nameString)) {
+				return; //Error - not valid var name
+			}
+//			if (!Regex.isVarNameValid(nameString)) { // wo space in the end
+//				return; //Error
+//			}
+			if (valueString == null) {
+				if (hasFinal) {
+					throw new UnInitializedFinalVar();
+				}
+				scope.addVariable (nameString, new Variable(false, false, type));
+			} else {
+				checkVarValue(valueString, type);
+				scope.addVariable(nameString, new Variable(true, hasFinal, type));
+			}
+		}
+	}
+
+	protected void checkVarValue(String valString, Keywords.Type type) throws UnmatchingValueError {
+		if (Regex.isValidVal(type.getRegex(), valString)) {
+			return;
+		}
+		throw new UnmatchingValueError();
 	}
 }
--- src/oop/ex6/parsers/GlobalParser.java
+++ src/oop/ex6/parsers/GlobalParser.java
@@ -10,18 +10,18 @@ public class GlobalParser extends ScopeParser {
 
 	private static final String FINAL = "final";
 	private static final String FIRST = "first";
-	private static final String BOOLEAN = "boolean";
-	private static final String INT = "int";
-	private static final String DOUBLE = "double";
-	private static final String STRING = "String";
-	private static final String CHAR = "char";
-	private final Global globalScope;
+//	private static final String BOOLEAN = "boolean";
+//	private static final String INT = "int";
+//	private static final String DOUBLE = "double";
+//	private static final String STRING = "String";
+//	private static final String CHAR = "char";
+//	private final Global globalScope;
 	private static GlobalParser globalParser;
 
 	//-------------------Singleton constructor & access---------------------\\
 	private GlobalParser() {
-		super(null);
-		globalScope = Global.getInstance();
+//		globalScope = Global.getInstance();
+		super(null, Global.getInstance());
 		//		Methods = new HashMap<>();
 	}
 
@@ -69,55 +69,55 @@ public class GlobalParser extends ScopeParser {
 	}
 
 
-	private void checkVarValue(String valString, Keywords.Type type) throws UnmatchingValueError {
-		if (Regex.isValidVal(type.getRegex(), valString)) {
-			return;
-		}
-		throw new UnmatchingValueError();
-	}
+//	private void checkVarValue(String valString, Keywords.Type type) throws UnmatchingValueError {
+//		if (Regex.isValidVal(type.getRegex(), valString)) {
+//			return;
+//		}
+//		throw new UnmatchingValueError();
+//	}
 
-	private Keywords.Type checkVarType(String firstWord) {
-		switch (firstWord) {
-		case BOOLEAN:
-			return Keywords.Type.BOOLEAN;
-		case CHAR:
-			return Keywords.Type.CHAR;
-		case STRING:
-			return Keywords.Type.STRING;
-		case INT:
-			return Keywords.Type.INT;
-		case DOUBLE:
-			return Keywords.Type.DOUBLE;
-		default:
-			return null;
-		}
-	}
+//	private Keywords.Type checkVarType(String firstWord) {
+//		switch (firstWord) {
+//		case BOOLEAN:
+//			return Keywords.Type.BOOLEAN;
+//		case CHAR:
+//			return Keywords.Type.CHAR;
+//		case STRING:
+//			return Keywords.Type.STRING;
+//		case INT:
+//			return Keywords.Type.INT;
+//		case DOUBLE:
+//			return Keywords.Type.DOUBLE;
+//		default:
+//			return null;
+//		}
+//	}
 
-	public void createVars(boolean hasFinal, Keywords.Type type, Regex regex)
-			throws UnInitializedFinalVar, UnmatchingValueError, IllegalFileFormat {
-		String[] varDeclarations = regex.splitByComma();
-		for (String declaration : varDeclarations) {
-			String[] str = Regex.getVarNameAndValue(declaration);
-			String nameString = str[0], valueString = str[1];
-			if (nameString == null) {
-				return; //Error - no var name
-			}
-			if (!Regex.isVarNameValid(nameString)) { // wo space in the end
-				return; //Error
-			}
-			if (valueString == null) {
-				if (hasFinal) {
-					throw new UnInitializedFinalVar();
-				}
-
-				globalScope.addVariable
-						(nameString, new Variable(false, false, type));
-			} else {
-				checkVarValue(valueString, type);
-				globalScope.addVariable(nameString, new Variable(true, hasFinal, type));
-			}
-		}
-	}
+//	public void createVars(boolean hasFinal, Keywords.Type type, Regex regex)
+//			throws UnInitializedFinalVar, UnmatchingValueError, IllegalFileFormat {
+//		String[] varDeclarations = regex.splitByComma();
+//		for (String declaration : varDeclarations) {
+//			String[] str = Regex.getVarNameAndValue(declaration);
+//			String nameString = str[0], valueString = str[1];
+//			if (nameString == null) {
+//				return; //Error - no var name
+//			}
+//			if (!Regex.isVarNameValid(nameString)) { // wo space in the end
+//				return; //Error
+//			}
+//			if (valueString == null) {
+//				if (hasFinal) {
+//					throw new UnInitializedFinalVar();
+//				}
+//
+//				scope.addVariable
+//						(nameString, new Variable(false, false, type));
+//			} else {
+//				checkVarValue(valueString, type);
+//				scope.addVariable(nameString, new Variable(true, hasFinal, type));
+//			}
+//		}
+//	}
 
 	public void createMethods() {
 		for (ScopeParser parser : childParsers) {
